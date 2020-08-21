6bef795c51caf0b62d1cd1ba69b69fd2d0fa8059
Steven Wood <stevenwood@Stevens-Macbook.attlocal.net>
2020-08-21 11:36:54 -0700

    divs created with JS, draw on mouseover, styles added

---
+++ sketch.js
@@ -0,0 +1,22 @@
+/* Global Variables */
+const container = document.querySelector('.container');
+
+makeGrid(16, 16);
+draw();
+
+function makeGrid(rows, columns) {
+  for (let i = 0; i < rows * columns; i++) {
+    const dot = document.createElement('div');
+    container.appendChild(dot);
+    dot.classList.add('grid-dot');
+  }
+}
+
+function draw() {
+  const dots = document.querySelectorAll('.grid-dot');
+  dots.forEach((dot) => {
+    dot.addEventListener('mouseover', () => {
+      dot.classList.add('draw');
+    });
+  });
+}
