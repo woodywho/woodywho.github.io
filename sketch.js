afbc56dc9981a3ffbb94f603f0b26de3f6103530
Steven Wood <stevenwood@Stevens-Macbook.attlocal.net>
2020-08-24 10:11:01 -0700

    finished version

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ makeGrid(16, 16);
 draw();
 
 function makeGrid(rows, columns) {
+  document.documentElement.style.setProperty('--grid-rows', rows);
+  document.documentElement.style.setProperty('--grid-columns', columns);
   for (let i = 0; i < rows * columns; i++) {
     const dot = document.createElement('div');
     container.appendChild(dot);
@@ -26,4 +28,16 @@ function shake() {
   dots.forEach((dot) => dot.classList.remove('draw'));
 }
 
+function resize() {
+  const newSize = prompt('Enter a number between 1 and 100:');
+  if (newSize != null && newSize <= 100 && newSize > 0) {
+    document.querySelectorAll('.grid-dot').forEach((item) => item.remove());
+    makeGrid(newSize, newSize);
+    draw();
+  } else {
+    alert('Please enter a valid number');
+  }
+}
+
 document.getElementById('shakeBtn').onclick = shake;
+document.getElementById('resizeBtn').onclick = resize;
